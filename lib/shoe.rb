15fa19ecb2a49e32df1d03f07d3c870c35ef008c
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-26 22:42:04 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,11 @@
 class Shoe 
-  def initialize()
+  def brand
+    @name 
+  end
+  def initialize(brand, color, type)
+    @brand = brand
+    @color = color
+    @type = type
+  end
+  
 # Make your shoe class here!
